34f891424e25cf30e8a3a5d2ff940d31a300ed0b
PriyaS2200 <sonkarpriya667@gmail.com>
2025-02-01 14:03:08 +0530

    Router updated

--- movies-app/src/main.jsx
+++ movies-app/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { ContextProvider } from './Context/ContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
